8629ab7c56742618cd52a880abbbe86f13897116
Julia Nash <contact@julianash.io>
2021-05-21 11:23:01 -0500

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,5 @@
-const mongoose     = require('mongoose');
-const validator = require('validator');
-
-
+const mongoose     = require('mongoose'),
+const validator    = require('validator');
 
 const userSchema = new mongoose.Schema ({
   name: {
